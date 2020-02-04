4ce287c46688391480b216252fe136e5876c012e
Sievers <casieve@s1048312.srn.sandia.gov>
2020-02-04 14:39:43 -0700

    Deleted old files. Set up class with parallel tools for easy comm passing. Set up general io module. Simplified main. Updated init. Created fitsnap class as backbone class. Started creating scraper parent class for future config scraping polymorphism.

--- fitsnap3/__init__.py
+++ fitsnap3/__init__.py
@@ -2,7 +2,10 @@
 # ## FitSNAP3
 # A Python Package For Training SNAP Interatomic Potentials for use in the LAMMPS molecular dynamics package
 #
-# _Copyright (2016) Sandia Corporation. Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain rights in this software. This software is distributed under the GNU General Public License_
+# _Copyright (2016) Sandia Corporation.
+# Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+# the U.S. Government retains certain rights in this software.
+# This software is distributed under the GNU General Public License_
 # ##
 #
 # #### Original author:
@@ -12,6 +15,7 @@
 # #### Key contributors (alphabetical):
 #     Mary Alice Cusentino (Sandia National Labs)
 #     Nicholas Lubbers (Los Alamos National Lab)
+#     Maybe me ¯\_(ツ)_/¯
 #     Adam Stephens (Sandia National Labs)
 #     Mitchell Wood (Sandia National Labs)
 #
@@ -24,61 +28,71 @@
 #     Laura Swiler (Sandia National Labs)
 #
 # <!-----------------END-HEADER------------------------------------->
-print("")
-print("    ______ _  __  _____  _   __ ___     ____  ")
-print("   / ____/(_)/ /_/ ___/ / | / //   |   / __ \ ")
-print("  / /_   / // __/\__ \ /  |/ // /| |  / /_/ /")
-print(" / __/  / // /_ ___/ // /|  // ___ | / ____/ ")
-print("/_/    /_/ \__//____//_/ |_//_/  |_|/_/      ")
-print("")
-print("-----------")
+
+try:
+    import mpi4py as mpi4py
+    from parallel_tools import pt
+
+    pt.single_print("numpy version: ", mpi4py.__version__)
+
+except Exception as e:
+    print("Trouble importing mpi4py package, exiting...")
+    raise e
+
+pt.single_print("")
+pt.single_print("    ______ _  __  _____  _   __ ___     ____  ")
+pt.single_print("   / ____/(_)/ /_/ ___/ / | / //   |   / __ \ ")
+pt.single_print("  / /_   / // __/\__ \ /  |/ // /| |  / /_/ /")
+pt.single_print(" / __/  / // /_ ___/ // /|  // ___ | / ____/ ")
+pt.single_print("/_/    /_/ \__//____//_/ |_//_/  |_|/_/      ")
+pt.single_print("")
+pt.single_print("-----------")
 try:
     import numpy as np
-    print("numpy version: ",np.__version__)
+    pt.single_print("numpy version: ", np.__version__)
 except Exception as e:
-    print("Trouble importing numpy package, exiting...")
+    pt.single_print("Trouble importing numpy package, exiting...")
     raise e
 
 try:
     import pandas as pd
-    print("pandas version: ",pd.__version__)
+    pt.single_print("pandas version: ", pd.__version__)
 except Exception as e:
-    print("Trouble importing pandas package, exiting...")
+    pt.single_print("Trouble importing pandas package, exiting...")
     raise e
 
 try:
     import sklearn as skl
-    print("scikit-learn version: ",skl.__version__)
+    pt.single_print("scikit-learn version: ", skl.__version__)
 except Exception as e:
-    print("Trouble importing scikit-learn package, exiting...")
+    pt.single_print("Trouble importing scikit-learn package, exiting...")
     raise e
 
 try:
     import scipy as sp
-    print("scipy version: ",sp.__version__)
+    pt.single_print("scipy version: ", sp.__version__)
 except Exception as e:
-    print("Trouble importing scipy package, exiting...")
+    pt.single_print("Trouble importing scipy package, exiting...")
     raise e
 
 try:
     import tqdm
-    print("tqdm version: ",tqdm.__version__)
+    pt.single_print("tqdm version: ", tqdm.__version__)
 except Exception as e:
-    print("Trouble importing tqdm package, exiting...")
+    pt.single_print("Trouble importing tqdm package, exiting...")
     raise e
 
 try:
     import natsort
-    print("natsort version: ",natsort.__version__)
+    pt.single_print("natsort version: ", natsort.__version__)
 except Exception as e:
-    print("Trouble importing natsort package, exiting...")
+    pt.single_print("Trouble importing natsort package, exiting...")
     raise e
-#try:
-#    import lammps
-#    print("LAMMPS version: ",lammps.version())
-#except Exception as e:
-#    print("Trouble importing LAMMPS library, exiting...")
-#    raise e
-print("-----------")
 
-from . import bispecopt, deploy, geometry, linearfit, runlammps, scrape, serialize
+# try:
+#     import lammps
+#     print("LAMMPS version: ",lammps.version())
+# except Exception as e:
+#     print("Trouble importing LAMMPS library, exiting...")
+#     raise e
+pt.single_print("-----------")

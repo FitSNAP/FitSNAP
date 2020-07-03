84644f8cd9110cfb27a0186797af82d28235da08
Sievers <casieve@s1048312.srn.sandia.gov>
2020-07-03 11:56:15 -0700

    Add transpose option

--- fitsnap3/solvers/svd.py
+++ fitsnap3/solvers/svd.py
@@ -1,5 +1,6 @@
 from fitsnap3.solvers.solver import Solver
 from fitsnap3.parallel_tools import pt
+from fitsnap3.io.input import config
 from scipy.linalg import lstsq
 import numpy as np
 
@@ -16,6 +17,8 @@ class SVD(Solver):
             testing = len(pt.shared_arrays['w'].array)
         w = pt.shared_arrays['w'].array[:testing]
         aw, bw = w[:, np.newaxis] * pt.shared_arrays['a'].array[:testing], w * pt.shared_arrays['b'].array[:testing]
-        bw = aw.T@bw
-        aw = aw.T@aw
+        # Transpose method does not work with Quadratic SNAP (why?)
+        if config.sections['SOLVER'].apply_transpose:
+            bw = aw.T@bw
+            aw = aw.T@aw
         self.fit, residues, rank, s = lstsq(aw, bw, 1.0e-13)

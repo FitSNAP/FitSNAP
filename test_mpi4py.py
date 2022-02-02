99ae5dfb2e0501b60f28af0067788f0a93897ee8
Sievers <charliesievers@cox.net>
2022-02-02 11:51:58 -0800

    workflow testing

--- test_mpi4py.py
+++ test_mpi4py.py
@@ -1,10 +1,13 @@
 from mpi4py import MPI
-import sys
+from subprocess import run
 
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-print(MPI.__file__, "on rank", rank)
-print(sys.executable, "on rank", rank)
+result = run(['ldd', '{}'.format(MPI.__file__)], stdout=PIPE)
+print(result)
+result = result.stdout.decode('utf-8').replace('\t', '').split('\n')[1:]
+print(result)
+print([string for string in result if 'libmpi' in string][0].split()[2])

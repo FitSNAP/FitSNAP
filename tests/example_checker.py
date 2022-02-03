6b8770b12d9f782266160c418b4eaf7be1cbe914
Sievers <charliesievers@cox.net>
2022-02-02 22:09:05 -0800

    workflow testing

--- tests/example_checker.py
+++ tests/example_checker.py
@@ -191,7 +191,7 @@ def mpi_run(nprocs, nnodes=None):
 					output = process.stdout
 					print(process.stdout, process.stderr)
 					if process.returncode:
-						raise CalledProcessError
+						raise CalledProcessError(process.returncode, process.args)
 					if 'Trouble reading input, exiting...' in output:
 						raise RuntimeError('Trouble reading input, exiting...')
 					with open("completed_process", "w") as fp:

635ff7d8105bfbfb0c5b2026ff21c4d4a7459be0
Sievers <charliesievers@cox.net>
2022-02-02 16:51:14 -0800

    workflow testing

--- tests/example_checker.py
+++ tests/example_checker.py
@@ -113,18 +113,18 @@ class MPICheck:
 		""" Find mpirun, mpiexec, or orterun"""
 		mpilib = Path(self._dylib_reader())
 		mpibin = None
-		for i in range(1,len(mpilib.parts)):
-			if mpilib.parts[-i]=='lib':
-				mpibin = mpilib.parents[i-1] / 'bin' 
-				break
+	#	for i in range(1,len(mpilib.parts)):
+	#		if mpilib.parts[-i]=='lib':
+	#			mpibin = mpilib.parents[i-1] / 'bin' 
+	#			break
 		if mpibin is None:
 			mpibin = mpilib.parent.parent / 'bin'
 		mpirun = str(mpibin / 'mpirun') if (mpibin / 'mpirun').exists() else None
 		mpiexec = str(mpibin / 'mpiexec') if (mpibin / 'mpiexec').exists() else None
 		orterun = str(mpibin / 'orterun') if (mpibin / 'orterun').exists() else None
-		# if mpirun is None and mpiexec is None and orterun is None:
-		# 	mpirun = check_output(['which', 'mpirun'], universal_newlines=True).strip()
-		# print(mpirun)
+		if mpirun is None and mpiexec is None and orterun is None:
+			mpirun = check_output(['which', 'mpirun'], universal_newlines=True).strip()
+		print(mpirun)
 		# assert Path(mpirun).exists()
 		assert mpirun is not None or mpiexec is not None or orterun is not None
 		return mpirun, mpiexec, orterun

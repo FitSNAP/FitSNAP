e24783371ded6c8079802d3bafd2f75ca0e8e541
mitwood <mitwood@sandia.gov>
2022-06-17 11:02:27 -0600

    Update lammps_snap.py

--- fitsnap3/calculators/lammps_snap.py
+++ fitsnap3/calculators/lammps_snap.py
@@ -109,7 +109,7 @@ class LammpsSnap(Calculator):
         number_of_atoms = len(self._data["AtomTypes"])
         positions = self._data["Positions"].flatten()
         elem_all = [config.sections["BISPECTRUM"].type_mapping[a_t] for a_t in self._data["AtomTypes"]]
-        self._lmp.command(f"create_atoms 1 random {number_of_atoms} 12345 1")
+        self._lmp.command(f"create_atoms 1 random {number_of_atoms} 12345 NULL")
         self._lmp.scatter_atoms("x", 1, 3, (len(positions) * ctypes.c_double)(*positions))
         self._lmp.scatter_atoms("type", 0, 1, (len(elem_all) * ctypes.c_int)(*elem_all))
         n_atoms = int(self._lmp.get_natoms())

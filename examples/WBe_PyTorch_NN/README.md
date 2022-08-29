c6c7df62d93aeebd75136945aba3c90ecddcb706
rohskopf <drew.rohskopf@gmail.com>
2022-08-29 14:31:52 -0600

    Allow variable number of descriptors for network inputs

---
+++ examples/WBe_PyTorch_NN/README.md
@@ -0,0 +1,21 @@
+## FitSnap3 NN Tungten - Beryllium example
+
+This example will generate a potential for tungsten and beryllium as published in 
+Wood, M. A., et. al. Phys. Rev. B 99 (2019) 184305.  This potential was developed 
+for the purpose of studying plasma material interactions in fusion reactors.
+
+#### Running this example:
+
+To run this example, use the following command in this directory:
+
+mpirun -n 1 python3 -m fitsnap3 WBe-example.in
+
+#### Files in this Directory
+
+WBe-example.in 
+
+Input file containing parameters to run FitSNAP and generate a SNAP-NN potential
+
+JSON/
+
+Directory that contains all the training configurations.
--- fitsnap3lib/io/sections/solver_sections/pytorch.py
+++ fitsnap3lib/io/sections/solver_sections/pytorch.py
@@ -18,8 +18,14 @@ try:
             self._check_if_used("SOLVER", "solver", "SVD")
 
             self.layer_sizes = self.get_value("PYTORCH", "layer_sizes", "num_desc 512 512 1").split()
+            self.num_elements = self.get_value("PYTORCH", "num_elements", "1", "int")
+            # Section.num_desc is summed over all element types, e.g. twojmax=6 and ntypes=2 gives
+            # 60 descriptors, but our per-atom networks should only have 30 descriptors, so here
+            # we divide by number of types
+            # this therefore requires that all atom types have the same number of descriptors
             if self.layer_sizes[0] == "num_desc":
-                self.layer_sizes[0] = Section.num_desc
+                assert (Section.num_desc % self.num_elements == 0)
+                self.layer_sizes[0] = int(Section.num_desc/self.num_elements)
             self.layer_sizes = [int(layer_size) for layer_size in self.layer_sizes]
             self.learning_rate = self.get_value("PYTORCH", "learning_rate", "1.0E-2", "float")
             self.num_epochs = self.get_value("PYTORCH", "num_epochs", "10", "int")
@@ -29,7 +35,6 @@ try:
             self.force_weight = self.get_value("PYTORCH", "force_weight", "1.0", "float")
             self.training_fraction = self.get_value("PYTORCH", "training_fraction", "0.8", "float")
             self.multi_element_option = self.get_value("PYTORCH", "multi_element_option", "1", "int")
-            self.num_elements = self.get_value("PYTORCH", "num_elements", "1", "int")
             self.manual_seed_flag = self.get_value("PYTORCH", "manual_seed_flag", "False", "bool")
 
             self.save_state_output = self.check_path(self.get_value("PYTORCH", "save_state_output", "FitTorchModel"))

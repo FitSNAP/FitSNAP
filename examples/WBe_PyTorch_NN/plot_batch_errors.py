07bcb0e5a693139a171f88d0b17cbabb8845cb72
rohskopf <drew.rohskopf@gmail.com>
2022-11-03 17:44:50 -0600

    Working InP_PACE example

---
+++ examples/WBe_PyTorch_NN/plot_batch_errors.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 12
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+data = np.loadtxt("batch_loss.dat")
+
+plt.plot(data[:,0], data[:,1], 'ko', markersize=2.0)
+
+plt.xlabel("Batch number")
+plt.ylabel("Average loss in epoch")
+
+plt.show()
+
+
+        
+

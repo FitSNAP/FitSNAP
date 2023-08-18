398babdc33a267b481401d0b499dff70fdfd30ae
rohskopf <drew.rohskopf@gmail.com>
2023-08-18 11:22:24 -0600

    Use fs dict in SVD solver

--- fitsnap3lib/solvers/svd.py
+++ fitsnap3lib/solvers/svd.py
@@ -14,27 +14,37 @@ class SVD(Solver):
 
     def __init__(self, name, pt, config):
         super().__init__(name, pt, config)
-        #self.config = Config()
-        #self.pt = ParallelTools()
 
-    #@pt.sub_rank_zero
-    def perform_fit(self, a=None, b=None, w=None, trainall=False):
-        @self.pt.sub_rank_zero
-        def decorated_perform_fit(a=None, b=None, w=None):
-            pt = self.pt   
-            config = self.config
+    def perform_fit(self, a=None, b=None, w=None, fs_dict=None, trainall=False):
+        """
+        Perform fit on a linear system. If no args are supplied, will use fitting data in `pt.shared_arrays`.
 
-            if not trainall:
-                training = [not elem for elem in pt.fitsnap_dict['Testing']]
-            else:
+        Args:
+            a (np.array): Optional "A" matrix.
+            b (np.array): Optional Truth array.
+            w (np.array): Optional Weight array.
+            fs_dict (dict): Optional dictionary containing a `Testing` key of which A matrix rows should not be trained.
+            trainall (bool): Optional boolean declaring whether to train on all samples in the A matrix.
+
+        The fit is stored as a member `fs.solver.fit`.
+        """
+        pt = self.pt
+        # Only fit on rank 0 to prevent unnecessary memory and work.
+        if pt._rank == 0:
+
+            if fs_dict is not None:
+                training = [not elem for elem in fs_dict['Testing']]
+            elif trainall:
                 training = [True]*np.shape(a)[0]
+            else:
+                training = [not elem for elem in pt.fitsnap_dict['Testing']]
 
             if a is None and b is None and w is None:
                 w = pt.shared_arrays['w'].array[training]
                 aw, bw = w[:, np.newaxis] * pt.shared_arrays['a'].array[training], w * pt.shared_arrays['b'].array[training]
             else:
                 aw, bw = w[:, np.newaxis] * a[training], w * b[training]
-    #       Look into gradient based linear solvers as well.
+
             if 'EXTRAS' in self.config.sections and self.config.sections['EXTRAS'].apply_transpose:
                 if np.linalg.cond(aw)**2 < 1 / fi.epsilon:
                     bw = aw[:, :].T @ bw
@@ -42,7 +52,6 @@ class SVD(Solver):
                 else:
                     print("The Matrix is ill-conditioned for the transpose trick")
             self.fit, residues, rank, s = lstsq(aw, bw, 1.0e-13)
-        decorated_perform_fit(a,b,w)
 
     def _dump_a(self):
         np.savez_compressed('a.npz', a=self.pt.shared_arrays['a'].array)

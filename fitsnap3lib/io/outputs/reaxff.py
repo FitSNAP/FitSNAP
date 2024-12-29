977f8473517cb1873c30915cdef33b65092f1420
alphataubio <alphataubio@gmail.com>
2024-12-28 21:19:07 -0500

    Create reaxff.py

---
+++ fitsnap3lib/io/outputs/reaxff.py
@@ -0,0 +1,13 @@
+from fitsnap3lib.io.outputs.outputs import Output
+"""Methods you may or must override in new output"""
+
+
+class Reaxff(Output):
+
+    def __init__(self, name, pt, config):
+        super().__init__(name, pt, config)
+
+    # You must override this method
+    def output(self):
+        
+        pass

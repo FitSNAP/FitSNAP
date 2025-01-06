cac76d8601a3c9116a776dfbe0c1527ba66a61c5
alphataubio <alphataubio@gmail.com>
2025-01-06 01:24:52 -0500

    Update cmaes.py

--- fitsnap3lib/io/sections/solver_sections/cmaes.py
+++ fitsnap3lib/io/sections/solver_sections/cmaes.py
@@ -8,11 +8,7 @@ class CMAES(Section):
         super().__init__(name, config, pt, infile, args)
         self.allowedkeys = ['popsize', 'sigma']
         self._check_section()
-
         self.popsize = self.get_value("CMAES", "popsize", "10", "int")
         self.sigma = self.get_value("CMAES", "sigma", "0.1", "float")
-
         self.delete()
-
-
 

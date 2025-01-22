36aa489803dc6211db322cde9da80988ec65a15a
alphataubio <alphataubio@gmail.com>
2025-01-21 22:39:30 -0500

    Update cmaes.py

--- fitsnap3lib/solvers/cmaes.py
+++ fitsnap3lib/solvers/cmaes.py
@@ -16,12 +16,14 @@ def force_field_strings(strings):
     ff_strings = strings
 
 
-def loss_function_tuple(i_j):
+def loss_function_tuple(i_j_x):
 
-    d = reaxff_calculator.pt.fitsnap_dict["Data"][i_j[0]]
-    reaxff_calculator.force_field_string = ff_strings[i_j[1]]
-    LammpsReaxff.process_reaxff_config(reaxff_calculator, d)
-    return (d['ground_shared_index'], d['ground_relative_index'], i_j[0], i_j[1], d['predicted_energy'])
+    d = reaxff_calculator.pt.fitsnap_dict["Data"][i_j_x[0]]
+    #reaxff_calculator.change_parameters(i_j_x[2])
+    parameters = reaxff_calculator.pt.shared_arrays["parameters"].array[i_j_x[1]]
+    reaxff_calculator.change_parameters(parameters)
+    reaxff_calculator.process_reaxff_config(d)
+    return (d['ground_shared_index'], d['ground_relative_index'], i_j_x[0], i_j_x[1], d['predicted_energy'])
 
 
 class CMAES(Solver):
@@ -35,38 +37,34 @@ class CMAES(Solver):
 
     def parallel_loss_function(self, x_arrays):
 
-        ff_strings = list(self.executor.map(force_field_string, x_arrays))
-        fs = [self.executor.submit(force_field_strings, ff_strings) for _ in range(self.executor.num_workers)]
-        #wait(fs)
-
+        #print("\n\nx_arrays="); pprint(x_arrays)
+        self.pt.shared_arrays["parameters"].array[:] = np.row_stack(x_arrays)
+        #print("\n\nshared_array="); pprint(self.pt.shared_arrays["parameters"].array)
         all_data = self.pt.fitsnap_dict["Data"]
         x_data_pairs = itertools.product(range(len(x_arrays)),range(len(all_data)))
-        #tuples = [(j,i,ff_strings[i]) for i, j in x_data_pairs]
-        tuples = [(j,i) for i, j in x_data_pairs]
-
-        #for t in tuples: print(f"({t[0]}, {t[1]})")
-
-        parallel_results = self.executor.map(loss_function_tuple, tuples, unordered=False)
+        #tuples = [(i,j,x_arrays[j]) for j, i in x_data_pairs]
+        tuples = [(i,j) for j, i in x_data_pairs]
+        parallel_results = list(self.executor.map(loss_function_tuple, tuples,unordered=True))
+        sorted_results = sorted(parallel_results, key=lambda t: (t[0],t[3],t[2]))
 
         #print(f"self.pt.get_rank()={self.pt.get_rank()}")
 
-        #answer = [0.0] * len(x_arrays)
-        answer = []
+        answer = [0.0] * len(x_arrays)
+        #answer = []
 
-        for k, g in itertools.groupby(parallel_results, key=lambda t: (t[0],t[3])):
+        for k, g in itertools.groupby(sorted_results, key=lambda t: (t[0],t[3])):
 
             # (ground_shared_index, ground_relative_index, shared_index, x_index, computed_energy)
             group = list(g)
-            #print(f"\ngroup=")
-            #pprint(group)
+            #print(f"\nk={k} group="); pprint(group)
             predicted = np.array([t[4]-group[i+t[1]][4] for i, t in enumerate(group)])
             reference = np.array([all_data[t[2]]["Energy"] for t in group])
             weights = np.array([all_data[t[2]]["Weight"] for t in group])
-            weighted_residuals = weights * np.square((predicted - reference)/1.2550189475550748)
-            #answer[t[3]] += np.sum(weighted_residuals)
-            answer.append(np.sum(weighted_residuals))
+            #weighted_residuals = weights * np.square((predicted - reference)/1.2550189475550748)
+            weighted_residuals = np.square((predicted - reference))
+            answer[k[1]] += float(np.sum(weighted_residuals))
 
-        #pprint(answer)
+        #print(answer)
         return answer
 
 
@@ -84,10 +82,11 @@ class CMAES(Solver):
 
         #options={'maxiter': 99, 'maxfevals': 999, 'popsize': 3}
         options={
-          #'popsize': self.popsize, 'seed': 12345,
-          'popsize': 7, 'seed': 12345, 'maxiter': 5,
+          'popsize': self.popsize, 'seed': 12345, #'maxiter': 5,
           'bounds': [[p['range'][0] for p in self.parameters],[p['range'][1] for p in self.parameters]]
         }
+
+        self.pt.create_shared_array('parameters', self.popsize, len(self.parameters))
 
         if self.pt.stubs == 0:
             from mpi4py import MPI
@@ -107,7 +106,7 @@ class CMAES(Solver):
                 parallel_objective=self.parallel_loss_function, options=options)
 
         if self.pt._rank == 0:
-            self.fit = LammpsReaxff.change_parameters(reaxff_calculator,x_best)
+            self.fit = reaxff_calculator.change_parameters_string(x_best)
             self.errors = es.pop_sorted
 
         #cfun = cma.ConstrainedFitnessAL(self.loss_function, self.cmaes_constraints)

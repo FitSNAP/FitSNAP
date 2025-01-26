074400d2042ed26511e304e46d396dd23eff6f91
alphataubio <alphataubio@gmail.com>
2025-01-26 13:21:31 -0500

    Update cmaes.py

--- fitsnap3lib/solvers/cmaes.py
+++ fitsnap3lib/solvers/cmaes.py
@@ -7,42 +7,28 @@ from pprint import pprint
 from sys import exit
 
 
-def force_field_string(x):
-    return LammpsReaxff.change_parameters(reaxff_calculator,x)
-
-
-def force_field_strings(strings):
-    global ff_strings
-    ff_strings = strings
-
-
-def loss_function_tuple(i_j_x):
-
-    d = reaxff_calculator.pt.fitsnap_dict["Data"][i_j_x[0]]
-    #reaxff_calculator.change_parameters(i_j_x[2])
-    parameters = reaxff_calculator.pt.shared_arrays["parameters"].array[i_j_x[1]]
-    reaxff_calculator.change_parameters(parameters)
-    reaxff_calculator.process_reaxff_config(d)
-    return (d['ground_shared_index'], d['ground_relative_index'], i_j_x[0], i_j_x[1], d['predicted_energy'])
-
 def loss_function_subgroup(i_x_j):
 
     subgroup = reaxff_calculator.pt.fitsnap_dict["Data"][i_x_j[2]]
     reaxff_calculator.change_parameters(i_x_j[1])
     configs = subgroup['configs']
+    for c in configs: reaxff_calculator.process_reaxff_config(c)
 
-    for c in configs:
-        reaxff_calculator.process_reaxff_config(c)
+    if reaxff_calculator.energy:
+      ground_predicted_energy = configs[subgroup['ground_index']]['predicted_energy']
+      predicted_energy = np.array([c['predicted_energy']-ground_predicted_energy for c in configs])
+      weighted_residuals = subgroup['weights'] * np.square((predicted_energy - subgroup['reference_energy']))
 
-    ground_predicted_energy = configs[subgroup['ground_index']]['predicted_energy']
-    predicted_energy = np.array([c['predicted_energy']-ground_predicted_energy for c in configs])
-    weighted_residuals = subgroup['weights'] * np.square((predicted_energy - subgroup['reference_energy']))
+    if reaxff_calculator.force:
+      pass
+
     return (i_x_j[0], float(np.sum(weighted_residuals)))
 
 
 class CMAES(Solver):
 
     def __init__(self, name, pt, config):
+
         super().__init__(name, pt, config, linear=False)
         self.popsize = self.config.sections['CMAES'].popsize
         self.sigma = self.config.sections['CMAES'].sigma
@@ -54,46 +40,8 @@ class CMAES(Solver):
         all_data = self.pt.fitsnap_dict["Data"]
         x_subgroup_pairs = itertools.product(range(len(x_arrays)),range(len(all_data)))
         tuples = [(i,x_arrays[i],j) for i, j in x_subgroup_pairs]
-        parallel_results = self.executor.map(loss_function_subgroup, tuples, unordered=True)
-
         answer = [0.0] * len(x_arrays)
-
-        for p in parallel_results:
-          answer[p[0]] += p[1]
-
-        return answer
-
-
-    def parallel_loss_function(self, x_arrays):
-
-        #print("\n\nx_arrays="); pprint(x_arrays)
-        self.pt.shared_arrays["parameters"].array[:] = np.row_stack(x_arrays)
-        #print("\n\nshared_array="); pprint(self.pt.shared_arrays["parameters"].array)
-        all_data = self.pt.fitsnap_dict["Data"]
-        x_data_pairs = itertools.product(range(len(x_arrays)),range(len(all_data)))
-        #tuples = [(i,j,x_arrays[j]) for j, i in x_data_pairs]
-        tuples = [(i,j) for j, i in x_data_pairs]
-        parallel_results = list(self.executor.map(loss_function_tuple, tuples,unordered=True))
-        sorted_results = sorted(parallel_results, key=lambda t: (t[0],t[3],t[2]))
-
-        #print(f"self.pt.get_rank()={self.pt.get_rank()}")
-
-        answer = [0.0] * len(x_arrays)
-        #answer = []
-
-        for k, g in itertools.groupby(sorted_results, key=lambda t: (t[0],t[3])):
-
-            # (ground_shared_index, ground_relative_index, shared_index, x_index, computed_energy)
-            group = list(g)
-            #print(f"\nk={k} group="); pprint(group)
-            predicted = np.array([t[4]-group[i+t[1]][4] for i, t in enumerate(group)])
-            reference = np.array([all_data[t[2]]["Energy"] for t in group])
-            weights = np.array([all_data[t[2]]["Weight"] for t in group])
-            #weighted_residuals = weights * np.square((predicted - reference)/1.2550189475550748)
-            weighted_residuals = np.square((predicted - reference))
-            answer[k[1]] += float(np.sum(weighted_residuals))
-
-        #print(answer)
+        for p in self.executor.map(loss_function_subgroup, tuples, unordered=True): answer[p[0]] += p[1]
         return answer
 
 

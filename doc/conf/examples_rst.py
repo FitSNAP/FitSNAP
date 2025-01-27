966e772e89bbd38da9c4a3d5790684a44ebebd46
alphataubio <alphataubio@gmail.com>
2025-01-26 23:41:09 -0500

    Update examples_rst.py

--- doc/conf/examples_rst.py
+++ doc/conf/examples_rst.py
@@ -5,18 +5,20 @@ from itertools import islice
 with open('../src/examples.rst', 'w') as rst:
 
     print( '\nExamples\n=========\n\n', file=rst )
-
-    #.. include:: md/Compiling-and-running-on-a-local-computer.md
-    #  :parser: myst_parser.sphinx_
     
     with os.scandir('../../examples') as examples:
-        for e in sorted([e for e in examples if e.is_dir()], key=lambda e: e.name):
+
+        sorted_examples = sorted([e for e in examples if e.is_dir()], key=lambda e: e.name)
+        
+        for i, e in enumerate(sorted_examples):
             #print(e)
-            print(f"--------\n\n{e.name}\n{''.join(['-']*len(e.name))}\n", file=rst )
+
+            if i>0: print("--------\n", file=rst )
+            print(f"{e.name}\n{''.join(['-']*len(e.name))}\n", file=rst )
 
             if os.path.isfile(readme_path := f"../../examples/{e.name}/README.md"):
-                print(f".. include:: {readme_path}", file=rst )
-                print(f"  :parser: myst_parser.sphinx_\n", file=rst )
+                print(f".. include:: {readme_path}\n", file=rst )
+                #print(f"  :parser: myst_parser.sphinx_\n", file=rst )
             else:
                 print(f"See ``examples/{e.name}/`` for details.\n\n", file=rst )
             

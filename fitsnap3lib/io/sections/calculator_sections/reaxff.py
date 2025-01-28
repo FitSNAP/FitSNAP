450c42812dd6484b3aa8e367d2bf5a6f18d83866
alphataubio <alphataubio@gmail.com>
2025-01-28 02:47:04 -0500

    Update reaxff.py

--- fitsnap3lib/io/sections/calculator_sections/reaxff.py
+++ fitsnap3lib/io/sections/calculator_sections/reaxff.py
@@ -8,7 +8,7 @@ class Reaxff(Section):
         # let parent hold config and args
         super().__init__(name, config, pt, infile, args)
 
-        self.allowedkeys = ['force_field', 'parameters']
-        self.force_field = self.get_value("REAXFF", "force_field", "None", "str")
+        self.allowedkeys = ['potential', 'parameters']
+        self.potential = self.get_value("REAXFF", "potential", "None", "str")
         self.parameters = eval(self.get_value("REAXFF", "parameters", "None", "str"))
         self.delete()

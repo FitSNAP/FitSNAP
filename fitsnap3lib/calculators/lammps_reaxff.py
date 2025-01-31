1339b0ddc4b2a8d21a17f6277a9fa1254c6f0fb4
alphataubio <alphataubio@gmail.com>
2025-01-31 00:22:33 -0500

    Update lammps_reaxff.py

--- fitsnap3lib/calculators/lammps_reaxff.py
+++ fitsnap3lib/calculators/lammps_reaxff.py
@@ -40,11 +40,14 @@ class LammpsReaxff(LammpsBase):
         self._lmp.command("units real")
         self._lmp.command("atom_style charge")
         self._lmp.command("atom_modify map array sort 0 2.0")
+
+        # FIXME
         #xlo, ylo, zlo = np.min(self._data["Positions"],axis=0)-10.0
         #xhi, yhi, zhi = np.max(self._data["Positions"],axis=0)+10.0
         #print(xlo, ylo, zlo, xhi, yhi, zhi)
         #self._lmp.command(f'region box block {xlo} {xhi} {ylo} {yhi} {zlo} {zhi}')
         self._lmp.command(f"region box block -15 15 -15 15 -15 15")
+
         self._lmp.command(f"create_box {len(self.elements)} box")
         self._lmp.commands_list([f"mass {i+1} {self.masses[i]}" for i in range(len(self.masses))])
         self._lmp.command("pair_style reaxff NULL")
@@ -72,19 +75,14 @@ class LammpsReaxff(LammpsBase):
 
     def change_parameters(self, x):
 
-        for i, xi in enumerate(x):
-            self.parameters[i][-1] = float(xi)
-
-        #print(self.parameters)
-
-        self._lmp.set_reaxff_parameters(self.parameters)
+        self._lmp.set_reaxff_parameters(self.parameters, x)
 
 
     def _collect_lammps(self):
 
         if self.energy:
             predicted_energy = _extract_compute_np(self._lmp, "thermo_pe", 0, 0)
-            self._data['predicted_energy'] = predicted_energy if not np.isnan(predicted_energy) else 9999
+            self._data['predicted_energy'] = predicted_energy if not np.isnan(predicted_energy) else 99e99
             #print("_collect_lammps(self)...", self._i, self._data["Energy"], config_energy)
 
 
@@ -111,35 +109,42 @@ class LammpsReaxff(LammpsBase):
 
     def num_atoms_parameters_list(self, block):
 
-        if( block == 'GEN' ):
-            yield 0, ['']*33 + 'bond_softness' + ['']*5
+        if block == 'GEN':
+            yield 0
+            yield ['']*34 + ['bond_softness'] + ['']*7
 
-        if( block == 'ATM' ):
-            yield 1, [
+        elif block == 'ATM':
+            yield 1
+            yield [
                 'r_s', 'valency', 'mass', 'r_vdw', 'epsilon', 'gamma', 'r_pi', 'valency_e',
                 'alpha', 'gamma_w', 'valency_boc', 'p_ovun5', 'gauss_exp', 'chi', 'eta', 'p_hbond', 
                 'r_pi_pi', 'p_lp2', '', 'b_o_131', 'b_o_132', 'b_o_133', 'bcut_acks2', '', 
                 'p_ovun2', 'p_val3', '', 'valency_val', 'p_val5', 'rcore2', 'ecore2', 'acore2']
 
-        elif( block == 'BND' ):
-            yield 2, [
+        elif block == 'BND':
+            yield 2
+            yield [
                 'De_s','De_p','De_pp','p_be1','p_bo5','v13cor','p_bo6','p_ovun1',
                 'p_be2','p_bo3','p_bo4','','p_bo1','p_bo2','ovc','']
 
-        elif( block == 'OFD' ):
-            yield 2, ['D', 'r_vdW', 'alpha', 'r_s', 'r_p', 'r_pp']
+        elif block == 'OFD':
+            yield 2
+            yield ['D', 'r_vdW', 'alpha', 'r_s', 'r_p', 'r_pp']
 
-        elif( block == 'ANG' ):
-            yield 3, ['theta_00', 'p_val1', 'p_val2', 'p_coa1', 'p_val7', 'p_pen1', 'p_val4']
+        elif block == 'ANG':
+            yield 3
+            yield ['theta_00', 'p_val1', 'p_val2', 'p_coa1', 'p_val7', 'p_pen1', 'p_val4']
 
-        elif( block == 'TOR' ):
-            yield 4, ['V1', 'V2', 'V3', 'p_tor1', 'p_cot1', '', '']
+        elif block == 'TOR':
+            yield 4
+            yield ['V1', 'V2', 'V3', 'p_tor1', 'p_cot1', '', '']
 
-        elif( block == 'HBD' ):
-            yield 3, ['r0_hb', 'p_hb1', 'p_hb2', 'p_hb3']
+        elif block == 'HBD':
+            yield 3
+            yield ['r0_hb', 'p_hb1', 'p_hb2', 'p_hb3']
 
         else:
-            raise Exception(f"Block {block} not recognized, possible values are ATM, BND, OFD, ANG, TOR, HBD.")
+            raise Exception(f"Block {block} not recognized, possible values are GEN, ATM, BND, OFD, ANG, TOR, HBD.")
 
 
     def parameter_block(self, block, atoms):
@@ -149,24 +154,30 @@ class LammpsReaxff(LammpsBase):
         if( num_atoms != len(atoms) ):
             raise Exception(f"Block {block} expected {num_atoms} atoms, but {atoms} has {len(atoms)}.")
         
-        if( block == 'ATM' ):
-            atoms_string = ''.join(['\s*{:2}'.format(atoms[0]) for a in atoms])
+        if block == 'ATM':
+            atoms_string = r'\s*' + atoms[0]
             extra_indent = '\n   '
         else:
-            atoms_string = ''.join(['\s*'+str(self.elements.index(a)+1) for a in atoms])
+            atoms_string = ''.join([r'\s*'+str(self.elements.index(a)+1) for a in atoms])
             extra_indent = '\n      '
 
         pattern = fr'^{atoms_string}(?:\s+\-?[0-9]+\.[0-9]+){{{len(parameters_list)}}}'
 
         if( not (match := re.search(pattern, self.potential_string, flags=re.MULTILINE|re.DOTALL)) ):
             print("pattern...", pattern)
-            #print("self.potential_string...", self.potential_string)
+            print(f"--------\nself.potential_string...\n{self.potential_string}\n--------\n")
             raise Exception("Unable to match text to replace")
 
         return match.group(0)
 
 
     def parameter_value(self, block, atoms, name):
+
+        if block == 'GEN':
+            if name == 'bond_softness':
+                return float(self.potential_string.splitlines()[36].lstrip().split(' !')[0])
+            else:
+                raise NotImplementedError(f"GEN.{name} not implemented.")
 
         num_atoms, parameters_list = self.num_atoms_parameters_list(block)
         parameter_index = parameters_list.index(name)
@@ -176,48 +187,61 @@ class LammpsReaxff(LammpsBase):
     def _parse_parameters(self, config_parameters):
 
         self.parameters = []
+        self.values = []
 
         for p in config_parameters.split():
 
             # BND.H.O.p_be2
             tokens = p.split('.')
             p_block = tokens.pop(0)
-            p_block_index = ['ATM','BND','OFD','ANG','TOR','HBD'].index(p_block)
+            p_block_index = ['GEN','ATM','BND','OFD','ANG','TOR','HBD'].index(p_block)
             _, parameters_list = self.num_atoms_parameters_list(p_block)
             p_name = tokens.pop(-1)
             p_name_index = parameters_list.index(p_name)
             p_atom_types = [self.type_mapping[a] for a in tokens]
-            p_value = self.parameter_value(p_block, tokens, p_name)
-            parameter = [p_block_index] + p_atom_types + [p_name_index, p_value]
+            parameter = [p_block_index] + p_atom_types + [p_name_index]
             print(parameter)
             self.parameters.append(parameter)
+            self.values.append(self.parameter_value(p_block, tokens, p_name))
+
+
+    def change_general_parameter(self, name_index, value):
+
+        # GEN.bond_softness
+        if name_index == 34:
+            potential_string_lines = self.potential_string.splitlines(True)
+            potential_string_lines[name_index+2] = ' {:12.8f} ! GEN.bond_softness'.format(value)
+            self.potential_string = ''.join(potential_string_lines)
+        else:
+            raise NotImplementedError(f"GEN.{name_index} not implemented.")
 
 
     def change_parameter_string(self, block_index, atom_types, name_index, value):
 
-        block = ['ATM','BND','OFD','ANG','TOR','HBD'][block_index]
+        block = ['GEN','ATM','BND','OFD','ANG','TOR','HBD'][block_index]
         num_atoms, parameters_list = self.num_atoms_parameters_list(block)
         atoms = [self.elements[t-1] for t in atom_types]
         parameter_block = self.parameter_block(block, atoms)
         tokens = parameter_block.split()
         tokens[num_atoms+name_index] = ' {:12.8f}'.format(value)
         tokens_formatted = tokens[num_atoms:]
-
         extra_indent = '\n   ' if block == 'ATM' else '\n      '
-
         if( len(parameters_list)>8 ): tokens_formatted.insert(8, extra_indent)
         if( len(parameters_list)>16 ): tokens_formatted.insert(17, extra_indent)
         if( len(parameters_list)>24 ): tokens_formatted.insert(26, extra_indent)
-        replacement = ' '.join(tokens[:num_atoms]) + ' ' + ' '.join(tokens_formatted) + '\n'
+        replacement = ' ' + ' '.join(tokens[:num_atoms]) + ' ' + ' '.join(tokens_formatted) + '\n'
         #print(replacement)
         self.potential_string = self.potential_string.replace(parameter_block,replacement)
 
 
     def change_parameters_string(self, x):
 
-        for i in range(len(x)):
-            p = self.parameters[i]
-            self.change_parameter_string(p[0], p[1:-2], p[-2], p[-1])
+        for p, v in zip(self.parameters, x):
+
+            if p[0]==0:
+                self.change_general_parameter(p[-1], v)
+            else:
+                self.change_parameter_string(p[0], p[1:-1], p[-1], v)
 
         return self.potential_string
 

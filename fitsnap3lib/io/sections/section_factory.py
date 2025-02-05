eb63ca3d69e095ba0876ffa446dc3d3e153e7477
alphataubio <alphataubio@gmail.com>
2025-02-05 01:28:22 -0500

    Update section_factory.py

--- fitsnap3lib/io/sections/section_factory.py
+++ fitsnap3lib/io/sections/section_factory.py
@@ -6,6 +6,7 @@ from fitsnap3lib.io.sections.calculator_sections.calculator import Calculator
 from fitsnap3lib.io.sections.calculator_sections.ace import Ace
 from fitsnap3lib.io.sections.calculator_sections.basic_calculator import Basic
 from fitsnap3lib.io.sections.calculator_sections.custom import Custom
+from fitsnap3lib.io.sections.calculator_sections.inq import INQ
 from fitsnap3lib.io.sections.calculator_sections.reaxff import Reaxff
 from fitsnap3lib.io.sections.eshift import Eshift
 from fitsnap3lib.io.sections.trainshift import Trainshift

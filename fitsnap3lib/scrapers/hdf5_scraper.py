7105085e5af46a210c7c0f807323f770b26c07d3
alphataubio <alphataubio@gmail.com>
2025-03-22 14:55:00 -0400

    Update hdf5_scraper.py

--- fitsnap3lib/scrapers/hdf5_scraper.py
+++ fitsnap3lib/scrapers/hdf5_scraper.py
@@ -17,7 +17,7 @@ class HDF5(Scraper):
 
     # ----------------------------------------------------------------
 
-    def __init__(self, name, pt, config, filename="/Users/mitch/Dropbox/github/hmx/spice/spice_test.hd5"):
+    def __init__(self, name, pt, config, filename="/Users/mitch/Dropbox/github/hmx/spice/spice20.hd5"):
         super().__init__(name, pt, config)
         self.data = []
         self.filename = filename
@@ -31,11 +31,6 @@ class HDF5(Scraper):
             self.size = 1
         self.exclude_atomic_numbers = [3, 35, 53]
         self._lattice_margin = 10
-        self.allowed_symbols = {
-            1: "H", 6: "C", 7: "N", 8: "O", 9: "F",
-            11: "Na", 12: "Mg", 15: "P", 16: "S",
-            17: "Cl", 19: "K", 20: "Ca"
-        }
 
     # ----------------------------------------------------------------
 
@@ -102,6 +97,7 @@ class HDF5(Scraper):
 
         atomic_numbers = group["atomic_numbers"][()]
         formation_energy = group["formation_energy"][()]
+        dft_total_energy = group["dft_total_energy"][()]
         dft_total_gradient = group["dft_total_gradient"][()]
         mbis_charges = group["mbis_charges"][()]
         scf_dipoles = group["scf_dipole"][()]
@@ -111,9 +107,10 @@ class HDF5(Scraper):
             "File": f"{group.name}/{i}",
             "Positions": positions,
             "Energy": formation_energy[i] * HARTREE_TO_KCAL_MOL,
+            "dft_total_energy": dft_total_energy[i] * HARTREE_TO_KCAL_MOL,
             "Forces": dft_total_gradient[i] * FORCE_CONV,
-            "Charges": mbis_charges[i].squeeze().tolist(),
-            "Dipole": scf_dipoles[i] * BOHR_TO_ANGSTROM,
+            #"Charges": mbis_charges[i].squeeze().tolist(),
+            #"Dipole": scf_dipoles[i] * BOHR_TO_ANGSTROM,
             "AtomTypes": [atomic_number_to_symbol(n) for n in atomic_numbers],
             "NumAtoms": len(atomic_numbers),
             "Lattice": [[ax, bx, cx], [ay, by, cy], [az, bz, cz]],

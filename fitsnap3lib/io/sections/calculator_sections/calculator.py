520053447464977b442eb3be387f643fff46a390
alphataubio <alphataubio@gmail.com>
2025-03-22 14:54:32 -0400

    add charge

--- fitsnap3lib/io/sections/calculator_sections/calculator.py
+++ fitsnap3lib/io/sections/calculator_sections/calculator.py
@@ -10,15 +10,19 @@ class Calculator(Section):
     def __init__(self, name, config, pt, infile, args):
         super().__init__(name, config, pt, infile, args)
         #self.pt = ParallelTools()
-        self.allowedkeys = ['calculator', 'energy', 'per_atom_energy', 'force', 'stress', \
-                            'nonlinear', 'per_atom_scalar', 'charge_fix', 'dipole']
+        self.allowedkeys = ['calculator', 'nonlinear', 'per_atom_scalar', 'per_atom_energy', \
+                            'energy', 'force', 'stress', 'charge', 'dipole', \
+                            'fix_charge']
         self._check_section()
 
         self.calculator = self.get_value("CALCULATOR", "calculator", "LAMMPSSNAP")
         self.energy = self.get_value("CALCULATOR", "energy", "True", "bool")
         self.per_atom_energy = self.get_value("CALCULATOR", "per_atom_energy", "False", "bool")
         self.per_atom_scalar = self.get_value("CALCULATOR", "per_atom_scalar", "False", "bool")
-        self.charge_fix = self.get_value("CALCULATOR", "charge_fix", "None", "str")
+
+        # REAXFF
+        self.charge_fix = self.get_value("CALCULATOR", "fix_charge", "None", "str")
+        self.charge = self.get_value("CALCULATOR", "charge", "False", "bool")
         self.dipole = self.get_value("CALCULATOR", "dipole", "False", "bool")
 
         self.dee = self.check_path(self.get_value("CALCULATOR", "dee", "detailed_energy_errors.dat"))

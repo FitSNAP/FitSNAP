74325f4a075cf16aa9b67c8e7abdab0aea6e49da
alphataubio <alphataubio@gmail.com>
2025-03-23 07:07:29 -0400

    Update lammps_reaxff.py

--- fitsnap3lib/calculators/lammps_reaxff.py
+++ fitsnap3lib/calculators/lammps_reaxff.py
@@ -14,79 +14,90 @@ class LammpsReaxff(LammpsBase):
     # --------------------------------------------------------------------------------------------
 
     def __init__(self, name, pt, config):
+
         super().__init__(name, pt, config)
+
+        self.potential = self.config.sections["REAXFF"].potential
+        self.elements = self.config.sections["REAXFF"].elements
+        self.masses = self.config.sections["REAXFF"].masses
+        self.type_mapping = self.config.sections["REAXFF"].type_mapping
+        self.parameters = self.config.sections["REAXFF"].parameters
+        self.charge_fix = self.config.sections["CALCULATOR"].charge_fix
+
+        self.energy = self.config.sections["CALCULATOR"].energy
+        self.force = self.config.sections["CALCULATOR"].force
+        self.stress = self.config.sections["CALCULATOR"].stress
+        self.charge = self.config.sections["CALCULATOR"].charge
+        self.dipole = self.config.sections["CALCULATOR"].dipole
+
         self._lmp = None
         self.pt.check_lammps()
-
-        self.potential = self.config.sections['REAXFF'].potential
-        self.elements = self.config.sections['REAXFF'].elements
-        self.masses = self.config.sections['REAXFF'].masses
-        self.type_mapping = self.config.sections['REAXFF'].type_mapping
-        self.parameters = self.config.sections['REAXFF'].parameters
-        self.charge_fix = self.config.sections['CALCULATOR'].charge_fix
-        self._initialize_lammps()
+        self._initialize_lammps(0)
 
     # --------------------------------------------------------------------------------------------
 
     def __del__(self):
+
         self._lmp = self.pt.close_lammps()
         del self
 
     # --------------------------------------------------------------------------------------------
 
-    def process_configs_with_values(self, values):
-
-        for i, c in enumerate(self._configs):
-            self._data = c
-            #print(f"*** rank {self.pt._rank} ok 1")
-            self._lmp.command("delete_atoms group all")
-            self._create_atoms_helper(type_mapping=self.type_mapping)
-
-            for j, v in enumerate(values):
-                try:
-                    #print(f"*** rank {self.pt._rank} ok 4")
-                    self._lmp.set_reaxff_parameters(self.parameters, v)
-                    #print(f"*** rank {self.pt._rank} ok 5")
-                    self._lmp.command("run 0 post no")
-                    #print(f"*** rank {self.pt._rank} ok 6")
-                    self._collect_lammps(i,j)
-                    #print(f"*** rank {self.pt._rank} ok 7")
-                except Exception as e:
-                    raise e
-
-    # --------------------------------------------------------------------------------------------
-
     def allocate_per_config(self, configs: list):
+
+        if self.pt._rank == 0:
+            ncpn = self.pt.get_ncpn(0)
+            return
 
         len_configs = len(configs)
         self._configs = configs
-        np.set_printoptions(threshold=5, edgeitems=1)
+        #np.set_printoptions(threshold=5, edgeitems=1)
         #pprint(configs, width=99, compact=True)
         ncpn = self.pt.get_ncpn(len_configs)
         popsize = self.config.sections['SOLVER'].popsize
         #self.pt.create_shared_array('weights', ncpn, 1)
-        #
+
+        self.sum_energy_residuals = np.zeros(popsize)
 
         if self.energy:
             #self.pt.create_shared_array('energy', len_all_data, 1)
-            #self.pt.create_shared_array('energy_predicted', popsize, len_all_data)
-            self.eweight = configs[0].["eweight"]
+            self.eweight = configs[0]["eweight"]
             self.energy_predicted = np.zeros((popsize,len_configs))
             self.energy_reference = np.zeros((len_configs))
             for i, c in enumerate(configs):
                 self.energy_reference[i] = c["Energy"]
 
         if self.force:
-            self.fweight = configs[0].["fweight"]
+            self.fweight = configs[0]["fweight"]
             self.forces_predicted = np.zeros((popsize,len_configs,3))
-            #self.pt.create_shared_array('forces_reference', len_all_data, 1)
-            #self.pt.create_shared_array('forces_predicted', popsize, len_all_data)
-
 
     # --------------------------------------------------------------------------------------------
 
-    def _initialize_lammps(self, printlammps=0):
-        super()._initialize_lammps(printlammps=printlammps)
+    def process_configs_with_values(self, values):
+
+        self.sum_energy_residuals[:] = 0
+
+        for config_index, c in enumerate(self._configs):
+            self._data = c
+            #print(f"*** rank {self.pt._rank} ok 1b")
+            self._prepare_lammps()
+
+            for pop_index, v in enumerate(values):
+                try:
+                    #print(f"*** rank {self.pt._rank} ok 1c")
+                    self._lmp.set_reaxff_parameters(self.parameters, v)
+                    self._lmp.command("run 0 post no")
+                    self._collect_lammps(config_index,pop_index)
+
+                except Exception as e:
+                    print(f"*** rank {self.pt._rank} exception {e}")
+                    raise e
+
+        return self.sum_energy_residuals
+
+    # --------------------------------------------------------------------------------------------
+
+    def _prepare_lammps(self):
         self._lmp.command("clear")
         self._lmp.command("boundary p p p")
 
@@ -98,16 +109,19 @@ class LammpsReaxff(LammpsBase):
         self._lmp.command("atom_modify map array sort 0 2.0")
 
         # FIXME
-        #xlo, ylo, zlo = np.min(self._data["Positions"],axis=0)-10.0
-        #xhi, yhi, zhi = np.max(self._data["Positions"],axis=0)+10.0
+        xlo, ylo, zlo = np.min(self._data["Positions"],axis=0)-10.0
+        xhi, yhi, zhi = np.max(self._data["Positions"],axis=0)+10.0
         #print(xlo, ylo, zlo, xhi, yhi, zhi)
-        #self._lmp.command(f'region box block {xlo} {xhi} {ylo} {yhi} {zlo} {zhi}')
-        self._lmp.command(f"region box block -15 15 -15 15 -15 15")
+        self._lmp.command(f'region box block {xlo} {xhi} {ylo} {yhi} {zlo} {zhi}')
+        #self._lmp.command(f"region box block -15 15 -15 15 -15 15")
 
         self._lmp.command(f"create_box {len(self.elements)} box")
+        #self._lmp.command("delete_atoms group all")
+        self._create_atoms_helper(type_mapping=self.type_mapping)
         self._lmp.commands_list([f"mass {i+1} {self.masses[i]}" for i in range(len(self.masses))])
         self._lmp.command("pair_style reaxff NULL")
         self._lmp.command(f"pair_coeff * * {self.potential} {' '.join(self.elements)}")
+        self._create_charge()
         self._lmp.command(self.charge_fix)
         if self.dipole: self._lmp.command("compute dipole all dipole")
 
@@ -115,23 +129,36 @@ class LammpsReaxff(LammpsBase):
 
     def _collect_lammps(self, config_index, pop_index):
 
-        #dist = self._lmp.numpy.extract_compute('dist',LMP_STYLE_LOCAL,LMP_TYPE_VECTOR)
-        #q = self._lmp.numpy.extract_compute('charge',LMP_STYLE_ATOM,LMP_TYPE_VECTOR)
-
         if self.energy:
             pe = _extract_compute_np(self._lmp, 'thermo_pe', LMP_STYLE_GLOBAL, LMP_TYPE_SCALAR)
-            print(f"*** rank {self.pt._rank} pe {pe}")
+            #print(f"*** rank {self.pt._rank} config_index {config_index} e_ref {self.energy_reference[config_index]} pe {pe}")
             self.energy_predicted[pop_index][config_index] = pe
 
+            energy_residual = pe - self.energy_reference[config_index]
+            self.sum_energy_residuals[pop_index] += np.square(energy_residual)
+
+        #    return np.sum(weights * np.nan_to_num(
+
+
+
+            #if np.isnan(pe):
+            #    print(f"*** rank {self.pt._rank} config {self._data}")
+
+
         if self.force:
-            f = self._lmp.extract_atom('f', LMP_STYLE_ATOM, LMP_TYPE_ARRAY)
-            print(f"f {f}")
-            self.forces_predicted[pop_index][config_index] = f
+            forces = self._lmp.extract_atom('f', LMP_STYLE_ATOM, LMP_TYPE_ARRAY)
+            print(f"*** rank {self.pt._rank} forces {forces}")
+            self.forces_predicted[pop_index][config_index] = forces
+
+        if self.charge:
+            charges = self._lmp.extract_atom('q', LMP_STYLE_ATOM, LMP_TYPE_VECTOR)
+            print(f"*** rank {self.pt._rank} charges {charges}")
+            self.charges_predicted[pop_index][config_index] = charges
 
         if self.dipole:
-            dipole = _extract_compute_np(self._lmp,'dipole',LMP_STYLE_GLOBAL,LMP_TYPE_VECTOR)
-            self._data['predicted_dipole'] = dipole
-            #print(f"dipole {dipole}")
+            dipole = _extract_compute_np(self._lmp, 'dipole', LMP_STYLE_GLOBAL, LMP_TYPE_VECTOR)
+            print(f"*** rank {self.pt._rank} charges {charges}")
+            self.dipole_predicted[pop_index][config_index] = dipole
 
     # --------------------------------------------------------------------------------------------
 

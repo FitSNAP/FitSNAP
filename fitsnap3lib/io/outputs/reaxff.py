aa8373a3ded62e8301e824ba73c0501fe10c42aa
alphataubio <alphataubio@gmail.com>
2025-04-06 15:04:43 -0400

    Update reaxff.py

--- fitsnap3lib/io/outputs/reaxff.py
+++ fitsnap3lib/io/outputs/reaxff.py
@@ -1,25 +1,26 @@
 from fitsnap3lib.io.outputs.outputs import Output, optional_open
-import os
+import os, threading
 from datetime import datetime
 
 class Reaxff(Output):
 
+    # --------------------------------------------------------------------------------------------
+
     def __init__(self, name, pt, config):
         super().__init__(name, pt, config)
+        self._io_lock = threading.Lock()
 
-    # You must override this method
+    # --------------------------------------------------------------------------------------------
+
     def output(self, coeffs, errors):
+        def thread_safe_write():
+            with self._io_lock:
+                base, ext = os.path.splitext(self.config.sections["OUTFILE"].potential_name)
+                timestamp = datetime.now().strftime("%Y%m%d-%H%M")
+                with optional_open(f"{base}-{timestamp}{ext}", 'wt') as file:
+                    file.write(coeffs)
 
-        #print( errors )
+        if self.pt._rank == 0:
+            thread_safe_write()
 
-        @self.pt.rank_zero
-        def write_ff():
-            base, ext = os.path.splitext(self.config.sections["OUTFILE"].potential_name)
-            timestamp = datetime.now().strftime("%Y%m%d-%H%M")
-            #timestamp = ""
-            with optional_open(f"{base}-{timestamp}{ext}", 'wt') as file:
-                file.write(coeffs)
-
-        write_ff()
-
-
+    # --------------------------------------------------------------------------------------------

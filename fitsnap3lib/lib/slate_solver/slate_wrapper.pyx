0420288e63cbd6b8611d3b591ce05209d6051f9c
alphataubio <alphataubio@gmail.com>
2025-08-30 16:20:29 -0400

    Update slate_wrapper.pyx

--- fitsnap3lib/lib/slate_solver/slate_wrapper.pyx
+++ fitsnap3lib/lib/slate_solver/slate_wrapper.pyx
@@ -3,10 +3,11 @@
 
 import numpy as np
 cimport numpy as np
-from mpi4py import MPI
-from mpi4py.libmpi cimport MPI_Comm
 
 np.import_array()
+
+# Define MPI_Comm as a void pointer (opaque type)
+ctypedef void* MPI_Comm
 
 cdef extern from "slate/slate.hh" namespace "slate":
     void initialize() except +
@@ -16,15 +17,15 @@ cdef extern from "slate/slate.hh" namespace "slate":
 cdef extern from *:
     """
     extern "C" void slate_ridge_solve(double* local_ata, double* local_atb, double* solution,
-                                      int n, double alpha, MPI_Comm comm, int tile_size);
+                                      int n, double alpha, void* comm, int tile_size);
     """
     void slate_ridge_solve(double* local_ata, double* local_atb, double* solution,
-                          int n, double alpha, MPI_Comm comm, int tile_size) except +
+                          int n, double alpha, void* comm, int tile_size) except +
 
 def ridge_solve(np.ndarray[double, ndim=2, mode="c"] local_ata,
                 np.ndarray[double, ndim=1, mode="c"] local_atb,
                 double alpha,
-                MPI.Comm comm,
+                comm,
                 int tile_size=256):
     """
     Solve ridge regression using SLATE.
@@ -33,7 +34,7 @@ def ridge_solve(np.ndarray[double, ndim=2, mode="c"] local_ata,
         local_ata: Local A^T A matrix
         local_atb: Local A^T b vector
         alpha: Ridge parameter
-        comm: MPI communicator
+        comm: MPI communicator (mpi4py.MPI.Comm object)
         tile_size: SLATE tile size
     
     Returns:
@@ -41,9 +42,13 @@ def ridge_solve(np.ndarray[double, ndim=2, mode="c"] local_ata,
     """
     cdef int n = local_ata.shape[0]
     cdef np.ndarray[double, ndim=1, mode="c"] solution = np.zeros(n, dtype=np.float64)
-    cdef MPI_Comm c_comm = comm.ob_mpi
+    
+    # Get the MPI communicator handle at runtime
+    # Import mpi4py at runtime to avoid build-time dependency
+    from mpi4py import MPI
+    cdef size_t comm_ptr = MPI._handleof(comm)
     
     slate_ridge_solve(&local_ata[0,0], &local_atb[0], &solution[0], 
-                     n, alpha, c_comm, tile_size)
+                     n, alpha, <void*>comm_ptr, tile_size)
     
     return solution

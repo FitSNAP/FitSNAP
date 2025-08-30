2f854fdaeda74ed881b604f744b6de3355842dbc
alphataubio <alphataubio@gmail.com>
2025-08-30 12:41:30 -0400

    Update scalapack.py

--- fitsnap3lib/solvers/scalapack.py
+++ fitsnap3lib/solvers/scalapack.py
@@ -12,34 +12,99 @@ try:
             super().__init__(name, pt, config)
 
         def perform_fit(self):
-            # This dictionary is NoneType on other subranks.
+            # Check if we have a testing split on the head subrank of each node
+            has_testing = False
             if self.pt.get_subrank() == 0:
-                if any(self.pt.fitsnap_dict['Testing']):
-                    raise NotImplementedError("Testing w/ the ScaLAPACK solver is not implemented!")
-            self.pt.split_by_node(self.pt.shared_arrays['w'])
-            self.pt.split_by_node(self.pt.shared_arrays['a'])
-            self.pt.split_by_node(self.pt.shared_arrays['b'])
-            total_length = self.pt.shared_arrays['a'].get_total_length()
-            node_length = self.pt.shared_arrays['a'].get_node_length()
-            scraped_length = self.pt.shared_arrays['a'].get_scraped_length()
+                # Only rank 0 has the gathered fitsnap_dict with Testing info
+                if self.pt.get_rank() == 0:
+                    has_testing = ('Testing' in self.pt.fitsnap_dict and 
+                                   self.pt.fitsnap_dict['Testing'] is not None and 
+                                   any(self.pt.fitsnap_dict['Testing']))
+                # Broadcast from rank 0 to all head subnodes
+                if self.pt._number_of_nodes > 1:
+                    has_testing = self.pt._head_group_comm.bcast(has_testing, root=0)
+            
+            # Broadcast within each node
+            has_testing = self.pt._sub_comm.bcast(has_testing, root=0)
+            
+            # Handle testing/training split if needed
+            if has_testing:
+                if self.pt.get_subrank() == 0:
+                    # Get the Testing array and create training mask
+                    testing_array = None
+                    if self.pt.get_rank() == 0:
+                        testing_array = np.array(self.pt.fitsnap_dict['Testing'], dtype=bool)
+                    
+                    # Broadcast testing array to all head subnodes
+                    if self.pt._number_of_nodes > 1:
+                        testing_array = self.pt._head_group_comm.bcast(testing_array, root=0)
+                    
+                    training_mask = ~testing_array  # Invert to get training samples
+                    
+                    # Get original arrays
+                    a_orig = self.pt.shared_arrays['a'].array
+                    b_orig = self.pt.shared_arrays['b'].array
+                    w_orig = self.pt.shared_arrays['w'].array
+                    
+                    # Filter for training data only
+                    a_train = a_orig[training_mask]
+                    b_train = b_orig[training_mask]
+                    w_train = w_orig[training_mask]
+                    
+                    # Create new shared arrays with training data only
+                    train_length = len(a_train)
+                    n_features = a_orig.shape[1] if len(a_orig.shape) > 1 else 1
+                    
+                    self.pt.create_shared_array('a_train', train_length, n_features, 
+                                                 tm=self.config.sections["SOLVER"].true_multinode)
+                    self.pt.create_shared_array('b_train', train_length, 
+                                                 tm=self.config.sections["SOLVER"].true_multinode)
+                    self.pt.create_shared_array('w_train', train_length, 
+                                                 tm=self.config.sections["SOLVER"].true_multinode)
+                    
+                    # Copy training data to new arrays
+                    self.pt.shared_arrays['a_train'].array[:] = a_train
+                    self.pt.shared_arrays['b_train'].array[:] = b_train
+                    self.pt.shared_arrays['w_train'].array[:] = w_train
+                    
+                    # Store the training mask for error analysis later
+                    self.training_mask = training_mask
+            else:
+                # No testing split, use all data
+                self.pt.shared_arrays['a_train'] = self.pt.shared_arrays['a']
+                self.pt.shared_arrays['b_train'] = self.pt.shared_arrays['b']
+                self.pt.shared_arrays['w_train'] = self.pt.shared_arrays['w']
+                self.training_mask = None
+                    
+            # Now perform the fit with training data
+            self.pt.split_by_node(self.pt.shared_arrays['w_train'])
+            self.pt.split_by_node(self.pt.shared_arrays['a_train'])
+            self.pt.split_by_node(self.pt.shared_arrays['b_train'])
+            
+            total_length = self.pt.shared_arrays['a_train'].get_total_length()
+            node_length = self.pt.shared_arrays['a_train'].get_node_length()
+            scraped_length = self.pt.shared_arrays['a_train'].get_scraped_length()
             lengths = [total_length, node_length, scraped_length]
+            
             if self.pt.get_subrank() == 0:
-                w = self.pt.shared_arrays['w'].array[:]
-                aw, bw = w[:, np.newaxis] * self.pt.shared_arrays['a'].array[:], w * self.pt.shared_arrays['b'].array[:]
-                # TODO:
-                """
-                Transpose method does not work with Quadratic SNAP (why?)
-                We need to revisit this preconditioning of the linear problem, we can make this a bit more elegant.
-                Since this breaks some examples this will stay as a 'secret' feature.
-                Need to chat with some mathy people on how we can profile A and find good preconditioners.
-                Will help when we want to try gradient based linear solvers as well.
-                """
-                self.fit = lstsq(aw, bw, lengths=lengths)
+                w = self.pt.shared_arrays['w_train'].array[:]
+                aw = w[:, np.newaxis] * self.pt.shared_arrays['a_train'].array[:]
+                bw = w * self.pt.shared_arrays['b_train'].array[:]
+                
+                # Apply transpose trick if configured
+                if 'EXTRAS' in self.config.sections and self.config.sections['EXTRAS'].apply_transpose:
+                    if np.linalg.cond(aw)**2 < 1 / np.finfo(float).eps:
+                        bw = aw.T @ bw
+                        aw = aw.T @ aw
+                    else:
+                        if self.pt.get_rank() == 0:
+                            print("The Matrix is ill-conditioned for the transpose trick")
+                
+                self.fit = lstsq(aw, bw, self.pt, lengths=lengths)
                 if self.pt.get_subrank() == 0:
                     self.fit = self.pt.gather_to_head_node(self.fit)[0]
-                # self.fit, residues, rank, s = lstsq(aw, bw, 1.0e-13)
             else:
-                self.fit = dummy_lstsq()
+                self.fit = dummy_lstsq(self.pt)
 
         def _dump_a(self):
             np.savez_compressed('a.npz', a=self.pt.shared_arrays['a'].array)

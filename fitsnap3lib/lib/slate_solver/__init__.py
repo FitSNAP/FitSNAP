4c73a1e8535a50caae32149ce94597ba4b0ff11c
alphataubio <alphataubio@gmail.com>
2025-08-30 21:34:42 -0400

    Update __init__.py

--- fitsnap3lib/lib/slate_solver/__init__.py
+++ fitsnap3lib/lib/slate_solver/__init__.py
@@ -1,7 +1,7 @@
 """SLATE ridge solver module"""
 try:
-    from .slate_wrapper import ridge_solve
-    __all__ = ['ridge_solve']
+    from .slate_wrapper import ridge_solve_qr
+    __all__ = ['ridge_solve_qr']
 except ImportError:
-    ridge_solve = None
+    ridge_solve_qr = None
     __all__ = []

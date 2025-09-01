1bb3ff240467ee0fc3d4a678bb1758739b891d87
alphataubio <alphataubio@gmail.com>
2025-09-01 14:27:05 -0400

    Update ridge.py

--- fitsnap3lib/solvers/ridge.py
+++ fitsnap3lib/solvers/ridge.py
@@ -25,7 +25,7 @@ class RIDGE(Solver):
         # Only fit on rank 0 to prevent unnecessary memory and work.
         if pt._rank == 0:
         
-            np.set_printoptions(precision=3, suppress=True, linewidth=np.inf)
+            np.set_printoptions(precision=5, suppress=True, linewidth=np.inf)
 
             
             if fs_dict is not None:

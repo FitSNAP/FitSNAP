f3f2f706408e8028c7ecee168c300c56a6cf66f5
alphataubio <alphataubio@gmail.com>
2025-09-05 21:44:40 -0400

    Update hdf5_scraper.py

--- fitsnap3lib/scrapers/hdf5_scraper.py
+++ fitsnap3lib/scrapers/hdf5_scraper.py
@@ -111,9 +111,9 @@ class HDF5(Scraper):
                 #print(f"*** {group_name} formation_energy {formation_energy} weights {weights} {[atomic_number_to_symbol(n) for n in atomic_numbers]}")
                 #print(f"*** {group_name} {[atomic_number_to_symbol(n) for n in atomic_numbers]}")
 
-                # Divide conformations across all ranks
-                n_confs = 25 # conformations.shape[0]
-                for j in range(self.rank, n_confs, self.size):
+                # Add ALL configs, not just 80%
+                #for j in range(conformations.shape[0]):
+                for j in range(25):
                     self.local_configs.append((group_name, j))
 
         if self.pt.stubs==0:

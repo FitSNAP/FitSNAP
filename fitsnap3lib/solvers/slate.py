5e863eeec1d5a103bfd1ab3dacbba057f0421032
alphataubio <alphataubio@gmail.com>
2025-10-01 04:21:48 -0400

    cont

--- fitsnap3lib/solvers/slate.py
+++ fitsnap3lib/solvers/slate.py
@@ -42,7 +42,6 @@ class SLATE(SlateCommon):
     def perform_fit_ard(self):
         """
         
-        
         dtype = X.dtype
 
         n_samples, n_features = X.shape
@@ -55,7 +54,14 @@ class SLATE(SlateCommon):
         self.X_offset_ = X_offset_
         self.X_scale_ = X_scale_
 
- 
+        # Launch the convergence loop
+        keep_lambda = np.ones(n_features, dtype=bool)
+
+        lambda_1 = self.lambda_1
+        lambda_2 = self.lambda_2
+        alpha_1 = self.alpha_1
+        alpha_2 = self.alpha_2
+        verbose = self.verbose
 
         # Initialization of the values of the parameters
         eps = np.finfo(np.float64).eps
@@ -79,7 +85,46 @@ class SLATE(SlateCommon):
             if n_samples >= n_features
             else self._update_sigma_woodbury
         )
+        # Iterative procedure of ARDRegression
+        for iter_ in range(self.max_iter):
+            sigma_ = update_sigma(X, alpha_, lambda_, keep_lambda)
+            coef_ = update_coeff(X, y, coef_, alpha_, keep_lambda, sigma_)
 
+            # Update alpha and lambda
+            sse_ = np.sum((y - np.dot(X, coef_)) ** 2)
+            gamma_ = 1.0 - lambda_[keep_lambda] * np.diag(sigma_)
+            lambda_[keep_lambda] = (gamma_ + 2.0 * lambda_1) / (
+                (coef_[keep_lambda]) ** 2 + 2.0 * lambda_2
+            )
+            alpha_ = (n_samples - gamma_.sum() + 2.0 * alpha_1) / (sse_ + 2.0 * alpha_2)
+
+            # Prune the weights with a precision over a threshold
+            keep_lambda = lambda_ < self.threshold_lambda
+            coef_[~keep_lambda] = 0
+
+            # Compute the objective function
+            if self.compute_score:
+                s = (lambda_1 * np.log(lambda_) - lambda_2 * lambda_).sum()
+                s += alpha_1 * log(alpha_) - alpha_2 * alpha_
+                s += 0.5 * (
+                    fast_logdet(sigma_)
+                    + n_samples * log(alpha_)
+                    + np.sum(np.log(lambda_))
+                )
+                s -= 0.5 * (alpha_ * sse_ + (lambda_ * coef_**2).sum())
+                self.scores_.append(s)
+
+            # Check for convergence
+            if iter_ > 0 and np.sum(np.abs(coef_old_ - coef_)) < self.tol:
+                if verbose:
+                    print("Converged after %s iterations" % iter_)
+                break
+            coef_old_ = np.copy(coef_)
+
+            if not keep_lambda.any():
+                break
+
+        self.n_iter_ = iter_ + 1
 
         if keep_lambda.any():
             # update sigma and mu using updated params from the last iteration
@@ -108,8 +153,8 @@ class SLATE(SlateCommon):
 
         # Iterative procedure of ARDRegression
         for iter_ in range(self.max_iter):
-            sigma_ = update_sigma(X, alpha_, lambda_, lambda_mask)
-            coef_ = update_coeff(X, y, coef_, alpha_, lambda_mask, sigma_)
+            sigma_ = slate_ard_update_sigma_cython(X, alpha_, lambda_, lambda_mask)
+            coef_ = slate_ard_update_coeff_cython(X, y, coef_, alpha_, lambda_mask, sigma_)
 
             # Update alpha and lambda
             sse_ = np.sum((y - np.dot(X, coef_)) ** 2)
@@ -135,34 +180,49 @@ class SLATE(SlateCommon):
                 break
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # ARD iteration loop
-        converged = False
-        solution = None
         
+
+
+
+    # --------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """
                 
                 # Print progress
                 if iteration == 0 or iteration % 10 == 0 or converged:
@@ -203,6 +263,8 @@ class SLATE(SlateCommon):
                 
                 pt.all_print(f"*** ARD self.fit (first 20 coefficients) ------------------------\n"
                     f"{self.fit[:20]}\n-------------------------------------------------\n")
+
+        """
 
 
     # --------------------------------------------------------------------------------------------

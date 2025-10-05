1719c611e9d9afcd3341d35e8a0a8b98b9ea907b
alphataubio <alphataubio@gmail.com>
2025-10-05 14:37:42 -0400

    Update solver.py

--- fitsnap3lib/io/sections/solver_sections/solver.py
+++ fitsnap3lib/io/sections/solver_sections/solver.py
@@ -13,7 +13,7 @@ class Solver(Section):
         self._check_section()
 
         self.solver = self.get_value("SOLVER", "solver", "SVD")
-        self.multinode = self.solver.lower() in ["slate"]
+        self.multinode = self.solver.upper() in ["SLATE"]
         self.normalweight = self.get_value("SOLVER", "normalweight", "-12", "float")
         self.normratio = self.get_value("SOLVER", "normratio", "0.5", "float")
         self.compute_testerrs = self.get_value("SOLVER", "compute_testerrs", "0", "bool")

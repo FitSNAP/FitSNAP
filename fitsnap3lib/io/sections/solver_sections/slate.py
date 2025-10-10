b142a119993f3b8a1a2699cdb4a1e443cb632ced
alphataubio <alphataubio@gmail.com>
2025-10-10 19:40:21 -0400

    Update slate.py

--- fitsnap3lib/io/sections/solver_sections/slate.py
+++ fitsnap3lib/io/sections/solver_sections/slate.py
@@ -6,7 +6,9 @@ class Slate(Section):
     def __init__(self, name, config, pt, infile, args):
         super().__init__(name, config, pt, infile, args)
         self.allowedkeys = ['method', 'alpha',
-            'max_iter', 'tol', 'alpha_1', 'alpha_2', 'lambda_1', 'lambda_2', 'threshold_lambda']
+            'max_iter', 'tol', 
+            'alphabig', 'alphasmall', 'lambdabig', 'lambdasmall', 
+            'threshold_lambda', 'directmethod', 'scap', 'scai', 'logcut']
                        
         self._check_section()
         self._check_if_used("SOLVER", "solver", "SLATE")
@@ -17,22 +19,32 @@ class Slate(Section):
         # alpha for RIDGE
         self.alpha = self.get_value("SLATE", "alpha", "1.0E-8", "float")
 
+        # ARD parameters - matching legacy ARD section
         # Maximum number of iterations
         self.max_iter = self.get_value("SLATE", "max_iter", "300", "int")
         
         # Stop the algorithm if w has converged
         self.tol = self.get_value("SLATE", "tol", "1e-3", "float")
         
-        # Shape and inverse scale (rate) parameters for Gamma prior over alpha
-        self.alpha_1 = self.get_value("SLATE", "alpha_1", "1e-6", "float")
-        self.alpha_2 = self.get_value("SLATE", "alpha_2", "1e-6", "float")
-
-        # Shape and inverse scale (rate) parameters for Gamma prior over lambda
-        self.lambda_1 = self.get_value("SLATE", "lambda_1", "1e-6", "float")
-        self.lambda_2 = self.get_value("SLATE", "lambda_2", "1e-6", "float")
-
+        # Direct method hyperparameters (used if directmethod=1)
+        self.alphabig = self.get_value("SLATE", "alphabig", "1.0E-12", "float")
+        self.alphasmall = self.get_value("SLATE", "alphasmall", "1.0E-14", "float")
+        self.lambdabig = self.get_value("SLATE", "lambdabig", "1.0E-6", "float")
+        self.lambdasmall = self.get_value("SLATE", "lambdasmall", "1.0E-6", "float")
+        
         # Threshold for removing (pruning) weights with high precision from the computation.
-        self.threshold_lambda = self.get_value("SLATE", "threshold_lambda", "100", "float")
+        # If not specified, will be auto-computed as 10^(int(abs(log10(ap))) + logcut)
+        self.threshold_lambda = self.get_value("SLATE", "threshold_lambda", "0", "float")
+        
+        # Adaptive hyperparameter mode (0=adaptive using scap/scai, 1=direct using alphabig/lambdasmall)
+        self.directmethod = self.get_value("SLATE", "directmethod", "0", "int")
+        
+        # Scaling factors for adaptive hyperparameters (used if directmethod=0)
+        self.scap = self.get_value("SLATE", "scap", "1.e-3", "float")
+        self.scai = self.get_value("SLATE", "scai", "1.e-3", "float")
+        
+        # Log cutoff for auto-computing threshold_lambda (used if threshold_lambda not specified)
+        self.logcut = self.get_value("SLATE", "logcut", "0.3", "float")
 
         # Whether to calculate the intercept for this model. If set to false,
         # no intercept will be used in calculations (ie. data is expected to be centered)
